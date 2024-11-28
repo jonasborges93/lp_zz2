9695df3e0cf00a198c0879b97d9f2fed5525683e
jonasborges93 <jonas.bgs93@gmail.com>
2024-11-28 18:38:24 -0300

    fix: add link adm panel in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <footer class="footer">
     <div class="container">
         <div class="footer__content">
-            <img src="./assets/images/icon_pag.png" alt="Logomarca ZZ2" class="footer__logo">
+            <div class="footer__initial">
+                <img src="./assets/images/icon_pag.png" alt="Logomarca ZZ2" class="footer__logo">
+                <a href="./administracao.php" class="footer__link">Acessar Painel</a>
+            </div>
             <p class="footer__text"><?php echo date('Y'); ?> - Todos os Direitos Reservados</p>
             <div class="footer__creator">
                 <p class="footer__creator-text">Created at:</p>
