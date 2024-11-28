30c74446c33086a5f9dae038184489f9f36f04b9
jonasborges93 <jonas.bgs93@gmail.com>
2024-11-28 15:24:23 -0300

    feat: include footer

--- index.php
+++ index.php
@@ -473,6 +473,19 @@
       </div>
     </section>
 
+    <footer class="footer">
+      <div class="container">
+        <div class="footer__content">
+          <img src="./assets/images/icon_pag.png" alt="Logomarca ZZ2" class="footer__logo">
+          <p class="footer__text"><?php echo date('Y') ;?> - Todos os Direitos Reservados</p>
+          <div class="footer__creator">
+            <p class="footer__creator-text">Created at:</p>
+            <a href="https://jonasborges.dev" class="footer__creator-link" target="_blank">Jonas Borges</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
     <script src="./assets/scripts.js"></script>
   </body>
 </html>
